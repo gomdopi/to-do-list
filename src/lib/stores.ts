ab6f15916b7b7a93ad67cbafbb46f6c3d4526638
gomdopi <io.gomdopi@gmail.com>
2024-10-14 23:12:55 -0700

    organize imports

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,4 +1,4 @@
-import { writable } from 'svelte/store';
 import dbItems from '$lib/database.js';
+import { writable } from 'svelte/store';
 
 export const fakeDbItems = writable(dbItems);
--- test/App.test.ts
+++ test/App.test.ts
@@ -1,6 +1,6 @@
-import { render, screen } from '@testing-library/svelte';
 import App from '@/App.svelte';
-import { describe, it, expect } from 'vitest';
+import { render, screen } from '@testing-library/svelte';
+import { describe, expect, it } from 'vitest';
 
 describe('App', () => {
 	it('renders successfully', () => {
