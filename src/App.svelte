67cb5a49b2af2ac4c52ace5efddfe98a64b08c74
gomdopi <io.gomdopi@gmail.com>
2024-10-14 23:35:25 -0700

    test commit

--- src/App.svelte
+++ src/App.svelte
@@ -7,7 +7,7 @@
 
 <div class="bg-slate-200 rounded-md border-slate-700 border-2 min-w-80 w-3/6 max-w-xl">
 	<div class="bg-slate-400 rounded-t-sm border-slate-800 border-b p-2">
-		<p class="text-3xl text-center">{name}</p>
+		<p class="text-3xl text-center">{name}test</p>
 	</div>
 	<div class="p-4">
 		<List listItems={$fakeDbItems} />
